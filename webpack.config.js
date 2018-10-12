246988df2575bc802e14ee9cc8f162f9ddbbc192
Vladimir Daniuk <vovand1996@gmail.com>
2018-10-12 20:22:13 +0300

    update(webpack.config): build

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/main.js",
   output: {
     pathinfo: true,
     path: path.resolve(__dirname, "dist"),
-    publicPath: "./dist/",
+    publicPath: "./",
     filename: "bundle.js"
   },
   module: {
@@ -18,5 +21,24 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: "./media",
+        to: "./media",
+        toType: "dir"
+      }
+    ]),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "index.html",
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+      }
+    })
+  ]
 };
